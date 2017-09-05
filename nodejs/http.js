7dc8901a6c2caa6c55412375763b77f3eabaf6e6
shiweiGit <597221342@qq.com>
2017-09-05 12:17:18 +0800

    new

--- nodejs/http.js
+++ nodejs/http.js
@@ -15,6 +15,16 @@ http.createServer(function(req, res){
  
 }).listen(3000);
 
+//获取get请求
+var http = require('http');
+var url = require('url');
+var util = require('util');
+ 
+http.createServer(function(req, res){
+    res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
+    res.end(util.inspect(url.parse(req.url, true)));
+}).listen(3000);
+
 
 //获取post 请求
 var http = require('http');
