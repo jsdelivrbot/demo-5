149dad905b32d28468251a9cc4d950ed03e7abc7
shiweiGit <597221342@qq.com>
2017-09-05 10:14:57 +0800

    new

--- nodejs/test.js
+++ nodejs/test.js
@@ -38,19 +38,37 @@ http.createServer(function (request, response) {
 // console.log("写入字节数 : "+  len);
 // 
 
-// 创建一个可读流
-var readerStream = fs.createReadStream('input.txt');
+// // 创建一个可读流
+// var readerStream = fs.createReadStream('input.txt');
 
-// 创建一个可写流
-var writerStream = fs.createWriteStream('output.txt');
+// // 创建一个可写流
+// var writerStream = fs.createWriteStream('output.txt');
 
-// 管道读写操作
-// 读取 input.txt 文件内容，并将内容写入到 output.txt 文件中
-readerStream.pipe(writerStream);
-console.log("程序执行完毕");
+// // 管道读写操作
+// // 读取 input.txt 文件内容，并将内容写入到 output.txt 文件中
+// readerStream.pipe(writerStream);
+// console.log("程序执行完毕");
 
-fs.createReadStream('input.txt')
-  .pipe(zlib.createGzip())
-  .pipe(fs.createWriteStream('input.txt.gz'));
+// fs.createReadStream('input.txt')
+//   .pipe(zlib.createGzip())
+//   .pipe(fs.createWriteStream('input.txt.gz'));
   
-console.log("文件压缩完成。");
+// console.log("文件压缩完成。");
+
+
+function printDir(){
+   console.log(__dirname);
+}
+// 两秒后执行以上函数
+// var t = setTimeout(printDir, 2000);
+// clearTimeout(t);
+// setInterval(printDir, 2000);
+
+// 输出当前目录
+console.log('当前目录: ' + process.cwd());
+
+// 输出当前版本
+console.log('当前版本: ' + process.version);
+
+// 输出内存使用情况
+console.log(process.memoryUsage());
