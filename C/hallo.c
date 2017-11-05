b0500c93299760cc84f7649ffb86aa5aa6264bc9
shiwei <597221342@qq.com>
2017-11-05 18:39:15 +0800

    c

---
+++ C/hallo.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+	
+	printf("23+46=%d\n",23+46);
+	printf("100-20=%d",100-20);
+	return 0;
+}  
---
+++ C/plus.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	int a;
+	int b;
+	
+	printf("请输入两个数值");
+	scanf("%d %d",&a,&b);
+	
+	printf("%d+%d=%d",a,b,a+b);
+	return 0; 
+}
+
+
